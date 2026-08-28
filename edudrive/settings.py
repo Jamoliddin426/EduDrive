ce435422100decdedf8822ef5ead2e6ff3b0d5aa
Jamoliddin426 <jaloliddinovmuhriddin10@gmail.com>
2026-08-28 13:29:04 +0500

    Update settings.py

--- edudrive/settings.py
+++ edudrive/settings.py
@@ -95,13 +95,13 @@ TIME_ZONE = "Asia/Tashkent"
 USE_I18N = True
 USE_TZ = True
 
-STATIC_URL = "static/"
-STATIC_ROOT = BASE_DIR / "staticfiles"
+STATIC_URL = "/static/"
+STATIC_ROOT = BASE_DIR / "/staticfiles"
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 STATIC_ROOT = BASE_DIR / "staticfiles"
 
-MEDIA_URL = "media/"
-MEDIA_ROOT = BASE_DIR / "media"
+MEDIA_URL = "/media/"
+MEDIA_ROOT = BASE_DIR / "/media"
 
 STROGES = {
     "staticfiles": {
